13964649d3d4f079512c955f73fd9f3afcaeda4b
whybe7 <whybe7@hotmail.com>
2023-04-18 01:25:14 +0200

    organize the `styles` in separate folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+// Styles: 
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
