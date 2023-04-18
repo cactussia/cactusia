ffada633f4eac89e11782434baf0a39c57581903
tchi-sama <tchisama41@gmail.com>
2023-04-18 21:17:04 +0000

    Refactor OrderNow component for responsive design and add message for free delivery
    - Refactored OrderNow component styles to be responsive
    - Added a message for free delivery throughout Morocco when buying three or more pots

--- src/components/OrderNow.jsx
+++ src/components/OrderNow.jsx
@@ -19,17 +19,18 @@ function OrderNow() {
     return potNum
   }
   return (
-    <div className="flex-1 flex justify-center items-center  h-full">
-      <div className="md:w-64 px-4 mb-5">
-        <p className="text-sm text-gray-600 bg-dark-white py-1 px-2 rounded-md my-3 w-fit">{getPotNum()} pot cactus</p>
-        <p className="text-base text-gray-600">Total Price </p>
+    <div className="flex-1 w-full flex justify-center items-center  h-full">
+      <div className="lg:w-64 px-4 mb-5 w-full">
+        
+        <p className="text-base text-gray-600">Total Price <span className="text-sm mx-2 text-gray-600 bg-dark-white2 border py-1 px-2 rounded-md my-3 w-fit">{getPotNum()} pot cactus</span></p>
         <h1 className="text-7xl text-green font-bold">{getPrice()} <span className="text-3xl">Dh</span></h1>
-        <hr className="bg-red-300 my-3" />
-        <p className="text-sm py-2 text-gray-600 bg-dark-white p-2 rounded-md my-3">
-          {" "}
-          dolor sit amet consectetur adipisicing elit. Cupiditate illo ve{" "}
+        <hr className=" my-3" />
+        {
+        <p className={(!(getPotNum()<3) && "h-0 py-0 my-0 ")+" duration-200 overflow-hidden text-sm py-2 text-gray-600 bg-dark-white p-2 rounded-md my-3"}>
+          When buying three or more, free delivery is available throughout Morocco !
         </p>
-        <button className="rounded-lg w-full md:w-fit hover:bg-green-dark duration-200 hover:scale-105 px-6 py-3 bg-green flex justify-center items-center gap-2 text-white font-normal text-lg">
+        }
+        <button className="rounded-lg w-full lg:w-fit hover:bg-green-dark duration-200 hover:scale-105 px-6 py-3 bg-green flex justify-center items-center gap-2 text-white font-normal text-lg">
           Order now
           <EastRoundedIcon/>
         </button>
