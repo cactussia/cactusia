6b9f9f815a9f5d2a56ac7a6567be23e458d74596
whybe7 <whybe7@hotmail.com>
2023-04-18 21:59:56 +0200

    update the structure

--- src/components/NavBarLink.jsx
+++ src/components/NavBarLink.jsx
@@ -3,18 +3,19 @@ import { useState, useEffect } from 'react';
 import { isActivePage } from '../utils';
 
 export default function NavBarLink({luncher, link, delay}) {
-  const [show, setShow] = useState(luncher)
+  const [mounted, setMounted] = useState(luncher);
 
+  // display each link after a delay
   useEffect(() => {
     const timer = setTimeout(() => {
-        setShow(luncher)
+      setMounted(luncher)
     }, delay);
     return () => clearTimeout(timer);
   }, [luncher]);
 
   return (
-    <li className={`relative h-16 w-80 flex items-center overflow-hidden transition-all ${isActivePage(link) ? "text-bleach" : "text-darker-green"} hover:text-bleach delay-[30] hover:ml-2`}>
-        <Link to={`${link.toLowerCase() == "home" ? "/" : "/"+link}`} className={`text-6xl absolute ${ show ? `top-0` : "top-80"} transition-all`}>{link}</Link>
+    <li className={`relative h-24 w-full flex items-center overflow-hidden duration-300 transition-all ${isActivePage(link) ? "text-bleach" : "text-darker-green"} hover:text-bleach hover:ml-2`}>
+        <Link to={`${link.toLowerCase() == "home" ? "/" : "/"+link}`} className={`text-8xl absolute ${ mounted ? `top-0` : "top-80"} transition-all`}>{link}</Link>
     </li>
   )
 }
