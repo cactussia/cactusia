45a569f43e97637cf37213d3ebc0f21c1c9e83ce
whybe7 <whybe7@hotmail.com>
2023-04-19 16:58:16 +0200

    using `section` over `div` for better symantic web and SEO

--- src/pages/Market.jsx
+++ src/pages/Market.jsx
@@ -4,12 +4,12 @@ import MarketBody from "../components/MarketBody";
 
 function Market() {
   return (
-    <div className=" lg:h-[100vh] w-[100vw] flex flex-col overflow-hidden relative ">
+    <section className=" lg:h-[100vh] w-[100vw] flex flex-col overflow-hidden relative ">
       <div className="background-market flex-1 flex-col flex">
         <NavBar />
         <MarketBody />
       </div>
-    </div>
+    </section>
   );
 }
 
