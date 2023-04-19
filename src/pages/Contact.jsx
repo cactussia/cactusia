bb53628834ad76c83bd7ae2eb4fd6c43d38b02ed
tchi-sama <tchisama41@gmail.com>
2023-04-19 18:56:21 +0000

    a

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,16 +3,10 @@ import NavBar from '../components/NavBar'
 
 function Contact() {
   return (
-<<<<<<< HEAD
-    <div className='h-[100vh]'>
-        <NavBar/>
-    </div>
-=======
     <>
       <NavBar/>
       <Footer/>
     </>
->>>>>>> 0bb86bd8af9d6c6b26372dfcda668ee76a7433b2
   )
 }
 
