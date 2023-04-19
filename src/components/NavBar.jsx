f30af78f7b5a0f53cb90c1168803db5cdb15584e
tchi-sama <tchisama41@gmail.com>
2023-04-19 02:04:38 +0000

    circle hand made added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,38 +1,69 @@
-import { useState } from 'react'
-import logo from "../assets/imags/logo.png"
-import { Link } from 'react-router-dom'
-import MenuRoundedIcon from '@mui/icons-material/MenuRounded'; 
-import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
-import { NavBarLinks } from '../utils';
-import NavBarLink from './NavBarLink';
+import { useState } from "react";
+import logo from "../assets/imags/logo.png";
+import { Link } from "react-router-dom";
+import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+import { NavBarLinks } from "../utils";
+import NavBarLink from "./NavBarLink";
+
+import handmade from "../assets/imags/handmade.png"
 
 function NavBar() {
-  const [open ,setOpen ]=useState(false)
+  const [open, setOpen] = useState(false);
   return (
-    <div className='container h-32 p-4 md:px-8  md:py-8 mx-auto flex justify-between items-start '>
-        <h1 className='text-4xl flex-1 font-bold text-green hidden lg:block'>MARKET</h1>
-        <div className='flex-1 flex lg:justify-center '>
-            <Link to={"/"}>
-                <img draggable={false} className='w-14 lg:w-20 mix-blend-darken' src={logo}/>
-            </Link>
-        </div>
-        <div className='flex-1 flex justify-end'>
-            <button onClick={()=>setOpen(p=>!p)} className={(open?" bg-dark-white ":" bg-green ")+' fixed w-12 h-12 md:w-16 md:h-16 hover:text-white rounded-full z-50 duration-200  hover:scale-105'}>
-              {
-                  open?
-                    <CloseRoundedIcon sx={{ color: "#768F6A", fontSize: 35 }}/>
-                   : 
-                    <MenuRoundedIcon sx={{ color: "#fff", fontSize: 30 }}/>
-              }
-            </button>
-        </div>
-        <div className={(open?" left-0 ":" left-[100vw] ")+' duration-300 delay-300 ease-out w-[100vw] h-[100vh] fixed bg-green-noise top-0 z-40 flex items-center justify-start'}>
-            <ul className='ml-4 md:ml-10 flex flex-col gap-6 justify-center items-start uppercase font-semibold'>
-                { NavBarLinks.map((link,index)=> <NavBarLink open={open} key={index} luncher={open} link={link.label} delay={link.delay}/>) }
-            </ul>
-        </div>
+    <div className="container h-32 p-4 md:px-8  md:py-8 mx-auto flex justify-between items-start ">
+      <h1 className="text-4xl flex-1 font-bold text-green hidden lg:block">
+        MARKET
+      </h1>
+      <div className="flex-1 flex lg:justify-center ">
+        <Link to={"/"}>
+          <img
+            draggable={false}
+            className="w-14 lg:w-20 mix-blend-darken"
+            src={logo}
+          />
+        </Link>
+      </div>
+      <div className="flex-1 flex justify-end">
+        <button
+          onClick={() => setOpen((p) => !p)}
+          className={
+            (open ? " bg-dark-white " : " bg-green ") +
+            " fixed w-12 h-12 md:w-16 md:h-16 hover:text-white rounded-full z-50 duration-200  hover:scale-105"
+          }
+        >
+          {open ? (
+            <CloseRoundedIcon sx={{ color: "#768F6A", fontSize: 35 }} />
+          ) : (
+            <MenuRoundedIcon sx={{ color: "#fff", fontSize: 30 }} />
+          )}
+        </button>
+      </div>
+      <div
+        className={
+          (open ? " left-0 " : " left-[100vw] ") +
+          " duration-300 delay-300 ease-out w-[100vw] h-[100vh] fixed bg-green-noise top-0 z-40 flex items-center justify-start"
+        }
+      >
+        <ul className="ml-4 md:ml-10 flex flex-col gap-6 justify-center items-start uppercase font-semibold">
+          {NavBarLinks.map((link, index) => (
+            <NavBarLink
+              open={open}
+              key={index}
+              luncher={open}
+              link={link.label}
+              delay={link.delay}
+            />
+          ))}
+        </ul>
+
+          <img className="2xl:w-[450px] xl:w-[420px] lg:w-[400px] md:w-[300px] w-[250px] drop-shadow-xl absolute -bottom-20 -right-20 animate-spining opacity-80" src={handmade} alt="" />
+
+
+
+      </div>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
--- src/components/NavBarLink.jsx
+++ src/components/NavBarLink.jsx
@@ -17,9 +17,10 @@ export default function NavBarLink({luncher, link, delay,open}) {
   },[open])
 
   return (
-    <li className={`relative h-12 sm:h-14 md:h-16 lg:h-32 w-[100vw] flex items-center overflow-hidden transition-all ${isActivePage(link) ? "text-bleach" : "text-darker-green"} hover:text-bleach delay-200 hover:ml-2`}>
+    <li className={`relative h-12 sm:h-14 md:h-16 lg:h-32 w-fit flex items-center overflow-hidden transition-all ${isActivePage(link) ? "text-bleach" : "text-darker-green"} hover:text-bleach delay-200 hover:ml-2`}>
         <div className={'md:w-6 md:h-6 w-4 h-4 rounded-full '+(isActivePage(link)?' bg-bleach ':"")}></div>
         <Link to={`${link.toLowerCase() == "home" ? "/" : "/"+link}`} className={`text-5xl sm:text-6xl md:text-7xl lg:text-9xl left-10 font-extrabold absolute ${ show ? `top-0` : "top-80"} transition-all`}>{link}</Link>
+        <Link to={`${link.toLowerCase() == "home" ? "/" : "/"+link}`} className={`text-5xl sm:text-6xl md:text-7xl lg:text-9xl left-10 font-extrabold opacity-0 mr-8 ${ show ? `top-0` : "top-80"} transition-all`}>{link}</Link>
     </li>
   )
 }
