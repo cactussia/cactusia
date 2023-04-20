febce4444615e1a05eb01f8d131134804c1ae444
why be <whybe7@hotmail.com>
2023-04-20 17:38:03 +0000

    Helper: Dynamic Copyright ©️🕹️
    Define a function tha dynamicly generate `copyright` sentenc.

--- src/utils/index.js
+++ src/utils/index.js
@@ -45,3 +45,28 @@ export const SocialMediaLinks = [{
 export function isActivePage(link){
   return window.location.pathname.toLowerCase() === "/"+link.toLowerCase();
 }
+
+/**
+ * `Price By Quantity` function to calculate the price based on the quantity
+ */
+export function getPriceByQte(qte){
+  switch (qte) {
+    case 1:  
+      return 65;
+    case 2:
+      return 129;
+    case 3:
+      return 195;
+    case 4:
+      return 249;
+    default:
+      return ((qte-4)*50)+249;
+  }
+}
+
+/**
+ * `Dynamic Copyright` function to display the current year and the current hostname
+ */
+export function dynamicCopyright(){
+  return `${window.location.hostname} © ${new Date().getFullYear()} - All Rights Reserved`;
+}
