9e96d9065013544808bd68b7c2bbd4a97ae4f1b7
why be <whybe7@hotmail.com>
2023-04-20 17:33:45 +0000

    * `SocialMediaLinks` is an array of objects that contains the links to be displayed in the footer

--- src/utils/index.js
+++ src/utils/index.js
@@ -20,6 +20,26 @@ export const NavBarLinks = [{
 ]
 
 /**
+ * `SocialMediaLinks` is an array of objects that contains the links to be displayed in the footer
+ */
+export const SocialMediaLinks = [{
+    label:"Facebook",
+    link:"https://www.facebook.com/",
+  },
+  {
+    label:"Instagram",
+    link:"https://www.instagram.com/",
+  },
+  {
+    label:"whatsapp",
+    link:"https://wa.me/0000000000",
+  },{
+    label:"Phone Number",
+    link:"tel:0000000000",
+  }
+]
+
+/**
  * `Active Page` function to track active page and compare it to the coresponded link
  */
 export function isActivePage(link){
