1c0d30b7258c1ae48f9ebab0b72f4c673caa8169
tchi-sama <tchisama41@gmail.com>
2023-05-20 22:20:41 +0100

    complate order

--- src/pages/ComplateOrder.jsx
+++ src/pages/ComplateOrder.jsx
@@ -17,9 +17,9 @@ function ComplateOrder() {
         <h1 className='text-4xl font-bold text-[#728b67] py-8'>Could you help us with some infos :)</h1>
         <p className='font-semibold text-[#728b67aa] '>those info will just help us to get u , u can send those info in whatsapp if u want</p>
         <div className='flex flex-col mt-10 gap-4'>
-            <input placeholder='your name' className='shadow-lg p-3 rounded-xl md:w-[400px] w-full font-semibold text-[#728b67] text-base border outline-[#728b67]'/>
-            <input placeholder='your phone' className='shadow-lg p-3 rounded-xl md:w-[400px] w-full font-semibold text-[#728b67] text-base border outline-[#728b67]'/>
-            <input placeholder='your address' className='shadow-lg p-3 rounded-xl md:w-[400px] w-full font-semibold text-[#728b67] text-base border outline-[#728b67]'/>
+            <input placeholder='your name' className='shadow-lg p-4 rounded-xl md:w-[400px] w-full font-semibold text-[#728b67] text-base border outline-[#728b67]'/>
+            <input placeholder='your phone' className='shadow-lg p-4 rounded-xl md:w-[400px] w-full font-semibold text-[#728b67] text-base border outline-[#728b67]'/>
+            <input placeholder='your address' className='shadow-lg p-4 rounded-xl md:w-[400px] w-full font-semibold text-[#728b67] text-base border outline-[#728b67]'/>
             <input type="button" value="complete" className='shadow-xl cursor-pointer p-2 px-8 rounded-xl md:w-fit w-full font-semibold text-white bg-[#728b67] text-lg  outline-[#728b67]'/>
         </div>
     </motion.div>
