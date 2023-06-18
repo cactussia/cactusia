4e30980fe6eee108bde4506cf570eb461074f3df
vvhybe <vvhybe@hotmail.com>
2023-06-18 17:50:01 +0100

    demo about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,6 +5,9 @@ function About() {
   return (
     <>
       <NavBar/>
+      <main className='container py-6 flex justify-center items-center bg-'>
+      <p className='max-w-4xl text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam itaque laboriosam, iste nulla dolores harum molestias fugit veritatis deleniti eaque eligendi reiciendis ratione saepe cupiditate odit, nesciunt perferendis voluptatibus? Illum?</p>
+      </main>
       <Footer/>
     </>
   )
