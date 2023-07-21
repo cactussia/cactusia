6bd264e4ca64eea7b937f736c8f83b2fc3b6c587
tchi-sama <tchisama41@gmail.com>
2023-07-21 19:49:04 +0100

    respnsive Watsapp btn

--- src/components/Desc.jsx
+++ src/components/Desc.jsx
@@ -1,9 +1,9 @@
 
 export default function Desc({title, text}) {
   return (
-    <section className='container flex justify-center items-center flex-col gap-8'>
+    <section className='container flex justify-center items-center flex-col gap-8 overflow-hidden'>
         <h1 className='sm:text-4xl text-center text-green text-4xl uppercase font-bold'>{title}</h1>
-        <p className='text-center sm:text-2xl px-8'>
+        <p className='text-center sm:text-2xl sm:px-12 px-8'>
         {text}
         </p>
   </section>
--- src/components/WhatsappBtn.jsx
+++ src/components/WhatsappBtn.jsx
@@ -3,7 +3,7 @@ import { SocialMediaLinks } from "../utils";
 
 export default function WhatsappBtn() {
   return (
-    <button className="fixed h-16 w-16 p-1 z-[9990] bottom-20 right-8 rounded-full flex items-center justify-center bg-bleach shadow-lg transition-all duration-150 hover:scale-[1.07]">
+    <button className="fixed sm:h-16 h-14 sm:w-16 w-14 p-1 z-[9990] bottom-8 sm:bottom-20 sm:right-8 right-4 rounded-full flex items-center justify-center bg-bleach shadow-lg transition-all duration-150 hover:scale-[1.07]">
         <a className="flex items-center justify-center" href={SocialMediaLinks[3].link} title={SocialMediaLinks[3].label} target="_blank" rel="noopener noreferrer">
             <WhatsApp className="scale-150 text-green-dark"/>
         </a>
