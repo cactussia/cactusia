55bddcbcec979a3b05a6931f53569b076fd1d010
tchisama <115560200+tchi-sama@users.noreply.github.com>
2023-07-23 23:56:29 +0100

    Update useLang.js
    SFG

--- src/store/useLang.js
+++ src/store/useLang.js
@@ -4,7 +4,7 @@ const langs=["en","fr"]
 const useLang = create((set) => ({
     lang:[],
     langs:langs,
-    langSelected:0,
+    langSelected:1,
     setLangSelected:(lang)=>set((state)=>({langSelected:state.langSelected==1?0:state.langSelected+1})),
     setLang: (data)=>set(()=>({lang:data}))
 }));
