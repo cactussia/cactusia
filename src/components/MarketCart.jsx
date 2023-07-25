22f15a226a0570842f530c6653bdeac2d9a6cba7
vvhybe <vvhybe@hotmail.com>
2023-07-25 18:33:19 +0100

    restyle the market cart to avoid overflow to the market

--- src/components/MarketCart.jsx
+++ src/components/MarketCart.jsx
@@ -8,6 +8,7 @@ import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import {motion} from "framer-motion"
 import { CardTravel, Shop } from '@mui/icons-material';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import CartIcon from './svg/CartIcon';
 
 function MarketCart() {
    const {pot,setPot,cactus,setCactus ,quantity,setQuantity,finalCactus,finalPots}=useContext(ControlersContext);
@@ -47,12 +48,12 @@ function MarketCart() {
    }, [currentItem])
    
   return (
-    <motion.div initial={{x:-200}} animate={{x:0}}  className='md:h-48 mb-2 flex flex-col items-center justify-center md:justify-center '>
-      <div className='xl:pt-12   overflow-auto px-4  max-w-full md:pt-8 pt-10  rounded-lg flex gap-2 '>
+    <motion.div initial={{x:-200}} animate={{x:0}}  className={`mb-2 py-8 flex flex-col items-center justify-center gap-4`}>
+      <div className={`xl:pt-12 pl-4 md:pt-10 pt-8 max-w-full rounded-lg flex gap-2 overflow-auto`}>
         {
           cart.map((item,key)=>{
             return(
-              <div key={key}  className={'flex relative flex-col items-center gap-1 drop-shadow-lg '}>
+              <div key={key}  className={'flex relative flex-col items-center gap-1 drop-shadow-lg'}>
               <button onClick={()=>handelSelect(key)} className={' w-20 h-20 bg-dark-white duration-100 rounded-lg   flex justify-center items-center flex-col border-2  '+(key==currentItem?" border-green " :"border-[#0001] ")}>
                 <div className={(key==currentItem?"scale-[1.25]":"scale-[1.20]")+' duration-150 relative -translate-y-5 w-[50px] flex flex-row justify-center items-center drop-shadow-md'}>
                     <img onContextMenu={e => e.preventDefault()} draggable={false} className={'h-[50px] absolute top-[10px] duration-150  pointer-events-none select-none'} src={key==currentItem?finalPots[pot]?.img:finalPots[item?.pot]?.img}></img>
@@ -73,14 +74,16 @@ function MarketCart() {
             )
           })
         }
-        <div className='w-20 h-20'>
-        <button onClick={handelAddNew} className='drop-shadow-lg w-20 h-20 duration-100 border-2 border-[#0001]  hover:bg-dark-white2 bg-dark-white rounded-lg  '>
-          <AddShoppingCartIcon className="text-green scale-[150%]" />
+      </div>
+      <div className={`w-full px-4 max-w-sm flex justify-center items-center transition-all`}>
+        <button onClick={handelAddNew} className='drop-shadow-lg w-full py-1 bg-green flex justify-center items-center gap-4 text-white font-semibold text-lg tracking-wider uppercase rounded-lg hover:bg-green-dark active:scale-90 transition-all'>
+          {/* <AddShoppingCartIcon className="text-green scale-[150%]" /> */}
+          <CartIcon width={45} height={45} target="button"/>
+          <span>Add to Cart</span>
         </button>
-        </div>
       </div>
     </motion.div>
   )
 }
 
-export default MarketCart
+export default MarketCart;
