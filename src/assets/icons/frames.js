f4cc8fd2a8129e22246542974e109ab659688d25
vvhybe <vvhybe@hotmail.com>
2023-08-05 22:45:45 +0100

    redesign thank page with animated icons as aframe of actions

---
+++ src/assets/icons/frames.js
@@ -0,0 +1,10 @@
+import F1 from './package.gif'
+import F2 from './route.gif'
+import F3 from './delivery-truck.gif'
+import F4 from './delivery-scooter.gif'
+import F5 from './delivery.gif'
+import F6 from './door.gif'
+import F7 from './unboxing.gif'
+
+const frames = [ F1, F2, F3, F4, F5, F6, F7 ];
+export default frames;
--- src/pages/Thank.jsx
+++ src/pages/Thank.jsx
@@ -1,13 +1,28 @@
 import NavBar from '../components/NavBar'
-import PlantIcon from '../components/svg/PlantIcon'
+// import PlantIcon from '../components/svg/PlantIcon'
+import frames from "../assets/icons/frames";
+import { useEffect, useState } from 'react';
 
 function Thank({name, onReturn}) {
+  const [frame, setFrame] = useState(0);
+  // const frame = useRef(0);
+
+  // swith to the next frame every 1s in a loop sequence
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setFrame((frame) => (frame + 1) % frames.length);
+      // frame.current = (frame.current + 1) % frames.length;
+    }, 1800);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <section className='min-h-screen md:px-8 flex flex-col px-4 py-6 container mx-auto'>
         <NavBar/>
         <div className='py-8 flex flex-1 flex-col items-center justify-center'>
             <div className="flex justify-center items-center flex-col gap-2 py-6">
-              <PlantIcon width={140} height={140} />
+              {/* <PlantIcon width={140} height={140} /> */}
+              <img src={frames[frame]} className='relative h-[140px] w-[140px] object-cover object-center aspect-square mix-blend-darken hue-rotate-[-90deg] saturate-[.5] grayscale-[10%] before:absolute before:top-0 before:left-0 before:h-full before:w-full before:bg-green-700 before:z-20' alt=""/>
               <h1 className='sm:text-6xl text-4xl uppercase font-semibold text-green text-center'>Merci "<span className='text-orange-800'>{name}</span>"  pour votre commande</h1>
             </div>
             <p className='sm:text-2xl text-xl  font-medium text-center'>Votre commande a bien été confirmée. Notre service prendra contact avec vous sous peu</p>
