cef9072efeab6a250542d17e6296c7b22335c43d
vvhybe <vvhybe@hotmail.com>
2023-09-16 13:28:33 +0100

    Scroll to the end of the cart container when a new item is added

--- src/components/MarketBody.jsx
+++ src/components/MarketBody.jsx
@@ -3,30 +3,23 @@ import PlantViewr from './PlantViewr'
 import OrderNow from './OrderNow'
 import MarketCart from './MarketCart'
 
-import { useMediaQuery } from 'react-responsive'
-
-
-
+// import { useMediaQuery } from 'react-responsive'
 
 function MarketBody() {
-
-
-  const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 1050px)'})
-  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1006px)' })
-
-
+  // const isDesktopOrLaptop = useMediaQuery({ query: '(min-width: 1050px)'})
+  // const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1006px)' })
 
   return (
-    <div className='flex flex-col flex-1 container mx-auto'>
-        <div className='py-8 flex justify-center items-center flex-col lg:flex-row '>
-            <div className='hidden flex-1 lg:flex'><MarketBodyControlers /></div>
-            <PlantViewr/>
-            <div className='flex flex-1 flex-col justify-center items-center lg:hidden'><MarketBodyControlers/><MarketCart/></div>
-             <OrderNow/>
+    <div className='flex -translate-y-10 flex-col flex-1 container mx-auto'>
+      <div className='py-2 flex justify-center items-center flex-col lg:flex-row'>
+        <div className='hidden flex-1 lg:flex'><MarketBodyControlers /></div>
+          <PlantViewr/>
+        <div className='flex flex-1 flex-col justify-center items-center lg:hidden'><MarketBodyControlers/><MarketCart/></div>
+          <OrderNow/>
         </div>
-            <div className='hidden  lg:block'><MarketCart/></div>
+        <div className='hidden  lg:block'><MarketCart/></div>
     </div>
   )
 }
 
-export default MarketBody
+export default MarketBody;
--- src/components/MarketCart.jsx
+++ src/components/MarketCart.jsx
@@ -1,4 +1,4 @@
-import { useContext , useEffect } from 'react'
+import { useContext , useEffect, useRef } from 'react'
 import AddRoundedIcon from "@mui/icons-material/AddRounded";
 import cactuses from "../assets/cactusImages/import"
 import pots from "../assets/potsImages/import"
@@ -11,74 +11,85 @@ import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import CartIcon from './svg/CartIcon';
 
 function MarketCart() {
-   const {pot,setPot,cactus,setCactus ,quantity,setQuantity,finalCactus,finalPots}=useContext(ControlersContext);
-   const {cart ,currentItem ,setCurrentItem ,setCart}= useContext(CartContext);
+  const {pot,setPot,cactus,setCactus ,quantity,setQuantity,finalCactus,finalPots}=useContext(ControlersContext);
+  const {cart ,currentItem ,setCurrentItem ,setCart}= useContext(CartContext);
 
-   const handelSelect = (key)=>{
-      setPot(cart[key].pot)
-      setCactus(cart[key].cactus)
-      setQuantity(cart[key].quantity)
-      setCurrentItem(key)
-   }
-   const handelAddNew = ()=>{
-          let key = cart.length
-          let newCart = cart
-          newCart.push({pot:0,cactus:0,quantity:1})
-          setCart(newCart)
-          setPot(cart[key].pot)
-          setCactus(cart[key].cactus)
-          setQuantity(cart[key].quantity)
-          setCurrentItem(key)
-   }
-   const handleRemove =(key)=>{
-        if(cart.length>1){
-          let newCart = cart.filter((item,i)=>key!==i)
-          setCart(newCart)
-          setCurrentItem(0)
-        }
-   }
-   useEffect(() => {
-        setPot(cart[currentItem].pot)
-        setCactus(cart[currentItem].cactus)
-        setQuantity(cart[currentItem].quantity)
-   }, [cart])
-   useEffect(() => {
-        setPot(cart[currentItem].pot)
-        setCactus(cart[currentItem].cactus)
-        setQuantity(cart[currentItem].quantity)
-   }, [currentItem])
+  const cartContainerRef = useRef(null);
+
+  // Scroll to the end of the cart container when a new item is added
+  useEffect(() => {
+    if (cartContainerRef.current) {
+      cartContainerRef.current.scrollLeft = cartContainerRef.current.scrollWidth;
+    }
+  }, [cart]);
+
+  const handelSelect = (key)=>{
+    setPot(cart[key].pot)
+    setCactus(cart[key].cactus)
+    setQuantity(cart[key].quantity)
+    setCurrentItem(key)
+  }
+  
+  const handelAddNew = ()=>{
+    let key = cart.length - 1
+    setCart([...cart,{pot:0,cactus:0,quantity:1}])
+    setCurrentItem(cart.length)
+    setPot(cart[key].pot)
+    setCactus(cart[key].cactus)
+    setQuantity(cart[key].quantity)
+  }
+
+  const handleRemove =(key)=>{
+    if(cart.length>1){
+      setCart(cart.filter((item,i)=>key!==i))
+      setCurrentItem(0)
+    }
+  }
+
+  useEffect(() => {
+    setPot(cart[currentItem].pot)
+    setCactus(cart[currentItem].cactus)
+    setQuantity(cart[currentItem].quantity)
+  }, [cart])
+
+  useEffect(() => {
+    setPot(cart[currentItem].pot)
+    setCactus(cart[currentItem].cactus)
+    setQuantity(cart[currentItem].quantity)
+  }, [currentItem])
    
   return (
-    <motion.div initial={{x:-200}} animate={{x:0}}  className={` mb-2 py-6 flex sm:flex-col flex-col-reverse items-center justify-center gap-4 overflow-y-visible`}>
-      <div 
-      className={`bg-bleach-light lg:bg-transparent xl:pt-8 md:pt-8 pt-8 pb-4 px-14 w-screen min-w-[450px] border-2 border-[#0001] lg:border-none rounded-lg flex ${ cart.length <= 4 && "justify-center" } items-center gap-2 overflow-x-auto overflow-y-visible transform transition-all`}
+    <motion.div initial={{x:-200}} animate={{x:0}}  className={`mb-2 py-4 flex flex-col-reverse items-center justify-center gap-2 overflow-y-visible`}>
+      <ul
+      ref={cartContainerRef}
+      className={`bg-bleach-light lg:bg-transparent xl:pt-6 md:pt-6 pt-6 pb-4 px-14 w-screen min-w-[450px] border-2 border-[#0001] lg:border-none rounded-lg flex ${ cart.length <= 4 && "justify-center" } lg:justify-center items-center gap-2 overflow-x-auto overflow-y-visible transform transition-all`}
       // className={`xl:pt-12 pl-4 md:pt-10 pt-8 max-w-full rounded-lg flex gap-2 overflow-x-auto overflow-y-visible`}
       >
         {
           cart.map((item,key)=>{
             return(
-              <div key={key} title={`${finalPots[item?.pot]?.name} Pot and ${finalCactus[item?.cactus]?.name} Cactus`} className={'flex relative flex-col items-center gap-1 drop-shadow-lg'}>
-              <button onClick={()=>handelSelect(key)} className={' w-20 h-20 bg-dark-white duration-100 rounded-lg   flex justify-center items-center flex-col border-2 overflow-y-visible  '+(key==currentItem?" border-green " :"border-[#0001] ")}>
-                <div className={(key==currentItem ?"scale-[1.25]":"scale-[1.20]")+' duration-150 relative -translate-y-5 w-[50px] flex flex-row justify-center items-center drop-shadow-md'}>
+              <li key={key} title={`${finalPots[item?.pot]?.name} Pot and ${finalCactus[item?.cactus]?.name} Cactus`} className={'flex relative flex-col items-center gap-1 drop-shadow-lg'}>
+                <button onClick={()=>handelSelect(key)} className={' w-20 h-20 bg-dark-white duration-100 rounded-lg   flex justify-center items-center flex-col border-2 overflow-y-visible  '+(key==currentItem?" border-green " :"border-[#0001] ")}>
+                  <div className={(key==currentItem ?"scale-[1.25]":"scale-[1.20]")+' duration-150 relative -translate-y-5 w-[50px] flex flex-row justify-center items-center drop-shadow-md'}>
                     <img onContextMenu={e => e.preventDefault()} draggable={false} className={'h-[50px] absolute top-[10px] duration-150  pointer-events-none select-none'} src={key==currentItem?finalPots[pot]?.img:finalPots[item?.pot]?.img}></img>
                     <img onContextMenu={e => e.preventDefault()} draggable={false} className='h-[50px] opacity-0 pointer-events-none select-none' src={key==currentItem?finalPots[pot]?.img:finalPots[item?.pot]?.img}></img>
                     <img onContextMenu={e => e.preventDefault()} draggable={false} className={'w-[50px] absolute top-[-18px] duration-150  pointer-events-none select-none'} src={key==currentItem? finalCactus[cactus]?.img: finalCactus[item?.cactus]?.img}></img>
-                </div>
-                {/* <p className='px-4 rounded-md font-medium text-gray-700 border-green bg-[#0001] '>
-                  {key==currentItem?quantity:item.quantity}
-                </p> */}
-              </button>
+                  </div>
+                  {/* <p className='px-4 rounded-md font-medium text-gray-700 border-green bg-[#0001] '>
+                    {key==currentItem?quantity:item.quantity}
+                  </p> */}
+                </button>
               {
-                  key==currentItem && cart.length>1 &&
-                  <button onClick={()=>handleRemove(key)} className={'z-[0] hover:bg-dark-white2 duration-100 bg-dark-white border border-gray-400 w-8 h-8 flex justify-center items-center rounded-full p-1'}>
-                    <CloseRoundedIcon sx={{color:"#444",fontSize:18}}/>
-                  </button>
+                key==currentItem && cart.length>1 &&
+                <button onClick={()=>handleRemove(key)} className={'z-[0] hover:bg-dark-white2 duration-100 bg-dark-white border border-gray-400 w-8 h-8 flex justify-center items-center rounded-full p-1'}>
+                  <CloseRoundedIcon sx={{color:"#444",fontSize:18}}/>
+                </button>
               }
-              </div>
+              </li>
             )
           })
         }
-      </div>
+      </ul>
       <div className={`w-screen flex justify-center items-center transition-all`}>
         <button onClick={handelAddNew} className='drop-shadow-lg w-full lg:max-w-screen-sm py-1 mx-4 bg-green flex justify-center items-center gap-4 text-white font-semibold text-lg tracking-wider uppercase rounded-lg hover:bg-green-dark active:scale-90 transition-all'>
           {/* <AddShoppingCartIcon className="text-green scale-[150%]" /> */}
