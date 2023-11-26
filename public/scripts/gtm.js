e57415e3a50683431c7d8ea26bd41bcdce0fa0e9
tchi-sama <tchisama41@gmail.com>
2023-11-26 19:06:14 +0100

    update GTM

--- public/scripts/gtm.js
+++ public/scripts/gtm.js
@@ -7,4 +7,4 @@
 	j.async = true;
 	j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
 	f.parentNode.insertBefore(j, f);
-})(window, document, "script", "dataLayer", "GTM-5RJJBBKZ");
+})(window, document, "script", "dataLayer", "GTM-PQ2K2Q85");
